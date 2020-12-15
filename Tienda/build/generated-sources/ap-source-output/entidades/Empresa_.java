0dcd047dd550999897d8441d58045a30148e68a3
Victor desktop <vflorespineda@gmail.com>
2020-12-14 20:08:53 -0600

    version actualizada

--- Tienda/build/generated-sources/ap-source-output/entidades/Empresa_.java
+++ Tienda/build/generated-sources/ap-source-output/entidades/Empresa_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-12-14T06:38:56")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-12-14T18:01:02")
 @StaticMetamodel(Empresa.class)
 public class Empresa_ { 
 
--- Tienda/build/generated-sources/ap-source-output/entidades/Factura_.java
+++ Tienda/build/generated-sources/ap-source-output/entidades/Factura_.java
@@ -10,7 +10,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-12-14T06:38:56")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-12-14T18:01:02")
 @StaticMetamodel(Factura.class)
 public class Factura_ { 
 
